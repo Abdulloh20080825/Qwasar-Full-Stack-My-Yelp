b29f5bf2eb2b0658463c2df8fe06b3f1b84a0323
Abdulloh20080825 <abdullohkurbonov2008@gmail.com>
2024-07-11 10:19:26 +0500

    edited cors policy

--- backend/app.js
+++ backend/app.js
@@ -11,11 +11,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
 
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
